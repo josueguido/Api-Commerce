28e314517d13d3f274585e38a2f022f4a465d34e
Josue Guido <josuguido@gmail.com>
2024-07-01 20:52:41 -0600

    feat: Initial commit of nearly completed API

---
+++ index.js
@@ -0,0 +1,88 @@
+import express from 'express'
+import jwt from 'jsonwebtoken'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { PORT, SECRET_JWT_KEY } from './config.js'
+import { UserRepository } from './user-repository.js'
+
+const app = express()
+
+app.set('view engine', 'ejs')
+
+const corsOptions = {
+  origin: 'http://localhost:5173', // URL del frontend
+  credentials: true // Permite el envío de cookies y encabezados de autorización
+}
+app.use(cors(corsOptions))
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use((req, res, next) => {
+  const token = req.cookies.access_token
+  req.session = { user: null }
+
+  try {
+    const data = jwt.verify(token, SECRET_JWT_KEY)
+    req.session.user = data
+  } catch (error) { }
+
+  next() // Seguir a la siguiente ruta o middleware
+})
+
+app.get('/', (req, res) => {
+  const { user } = req.session
+  res.render('index', user)
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body
+
+  try {
+    const user = await UserRepository.login({ username, password })
+    const token = jwt.sign(
+      { id: user._id, username: user.username }
+      , SECRET_JWT_KEY, {
+        expiresIn: '1h'
+      })
+
+    res
+      .cookie('access-token', token, {
+        httpOnly: true, // La Cookie solo se puede acceder desde el servidor
+        secure: process.env.NODE_ENV === 'production', // La Cookie solo se puede acceder en https
+        sameSite: 'strict', // Solo se puede acceder desde el mismo dominio
+        maxAge: 1000 * 60 * 60 // La Cookie solo tiene un tiempo de validez de una hora
+      })
+      .send({ user, token })
+  } catch (error) {
+    res.status(401).send(error.message)
+  }
+})
+
+app.post('/signup', async (req, res) => {
+  const { username, password } = req.body
+  console.log(req.body)
+
+  try {
+    const id = await UserRepository.create({ username, password })
+    res.send({ id })
+  } catch (error) {
+    res.status(400).send(error.message)
+  }
+})
+
+app.post('/logout', (req, res) => {
+  res
+    .clearCookie('access token')
+    .send({ message: 'logout successful' })
+})
+
+app.get('/proteted', (req, res) => {
+  const { user } = req.session
+  if (!user) return res.status(403).send('Access no authorized')
+  res.render('[proteted', user)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
---
+++ user-repository.js
@@ -0,0 +1,60 @@
+import DBLocal from 'db-local'
+import crypto from 'crypto'
+import bcrypt from 'bcrypt'
+import { SALT_ROUNDS } from './config.js'
+const { Schema } = new DBLocal({ path: './db' })
+
+const User = Schema('User', {
+  _id: { type: String, required: true },
+  username: { type: String, required: true },
+  password: { type: String, required: true }
+})
+
+export class UserRepository {
+  static async create ({ username, password }) {
+    Validation.username(username)
+    Validation.password(password)
+
+    // Asegurarse que el username no exista
+    const user = User.findOne({ username })
+    if (user) throw new Error('username already exists')
+
+    const id = crypto.randomUUID()
+    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+
+    User.create({
+      _id: id,
+      username,
+      password: hashedPassword
+    }).save()
+
+    return id
+  }
+
+  static async login ({ username, password }) {
+    Validation.username(username)
+    Validation.password(password)
+
+    const user = User.findOne({ username })
+    if (!user) throw new Error('usermane doesn`t exists')
+
+    const isValid = await bcrypt.compare(password, user.password)
+    if (!isValid) throw new Error('password is invalid')
+
+    const { password: _, ...publicUser } = user
+
+    return publicUser
+  }
+}
+
+class Validation {
+  static username (username) {
+    if (typeof username !== 'string') throw new Error('username must be a string')
+    if (username.length < 3) throw new Error('username must be at least 3 characters long')
+  }
+
+  static password (password) {
+    if (typeof password !== 'string') throw new Error('password must be a string')
+    if (password.length < 6) throw new Error('password must be at least 3 characters long')
+  }
+}
