14bec8e1d50c9b512cc290b8c315766d4efe1e23
Josue Guido <josuguido@gmail.com>
2024-07-18 20:04:51 -0600

    fix: cart purchase error fixed

--- controllers/payment.controllers.js
+++ controllers/payment.controllers.js
@@ -5,42 +5,76 @@ import { STRIPE_PRIVATE_KEY } from '../config.js'
 const stripe = new Stripe(STRIPE_PRIVATE_KEY)
 
 export const createSession = async (req, res) => {
-  const { productId } = req.body
+  const { productId, cart } = req.body
 
-  if (!productId) {
-    return res.status(400).json({ error: 'ID del producto es requerido' })
-  }
+  if (productId) {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/${productId}`)
+      const product = await response.json()
 
-  try {
-    const response = await fetch(`https://fakestoreapi.com/products/${productId}`)
-    const product = await response.json()
+      if (!product || Object.keys(product).length === 0) {
+        throw new Error('Could not get product')
+      }
 
-    if (!product || Object.keys(product).length === 0) {
-      throw new Error('No se pudo obtener el producto')
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              product_data: {
+                name: product.title,
+                description: product.description
+              },
+              currency: 'usd',
+              unit_amount: product.price * 100
+            },
+            quantity: 1
+          }
+        ],
+        mode: 'payment',
+        success_url: 'http://localhost:5173/success',
+        cancel_url: 'http://localhost:5173/cancel'
+      })
+
+      return res.json(session)
+    } catch (error) {
+      return res.status(500).json({ error: 'Error creating payment session' })
     }
+  } else if (cart && Array.isArray(cart) && cart.length > 0) {
+    try {
+      const lineItems = await Promise.all(cart.map(async (item) => {
+        const response = await fetch(`https://fakestoreapi.com/products/${item.id}`)
+        const product = await response.json()
 
-    const session = await stripe.checkout.sessions.create({
-      line_items: [
-        {
+        if (!product || Object.keys(product).length === 0) {
+          throw new Error('Could not get product')
+        }
+
+        return {
           price_data: {
+            currency: 'usd',
             product_data: {
               name: product.title,
               description: product.description
             },
-            currency: 'usd',
             unit_amount: product.price * 100
           },
-          quantity: 1
+          quantity: item.quantity
         }
-      ],
-      mode: 'payment',
-      success_url: 'http://localhost:5173/success',
-      cancel_url: 'http://localhost:5173/cancel'
-    })
+      }))
 
-    return res.json(session)
-  } catch (error) {
-    console.error('Error al crear la sesión de pago:', error)
-    return res.status(500).json({ error: 'Error al crear la sesión de pago' })
+      const session = await stripe.checkout.sessions.create({
+        line_items: lineItems,
+        mode: 'payment',
+        success_url: 'http://localhost:5173/success',
+        cancel_url: 'http://localhost:5173/cancel'
+      })
+
+      return res.json(session)
+    } catch (error) {
+      console.error(error)
+      return res.status(500).json({ error: 'Error creating payment session' })
+    }
+  } else {
+    return res.status(400).json({ error: 'ID product or cart is required' })
   }
 }
