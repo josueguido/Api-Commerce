cab196c098ad31ab8dc79d5276aba49023f01eb8
Josue Guido <josuguido@gmail.com>
2024-07-23 20:52:53 -0600

    fix: cors problem

--- index.js
+++ index.js
@@ -36,8 +36,9 @@ app.use(cookieParser())
 
 app.use((req, res, next) => {
   const origin = req.header('Origin')
-  if (ACCEPTED_ORIGIN.includes(origin)) {
+  if (ACCEPTED_ORIGIN.includes(origin) || !origin) {
     res.header('Access-Control-Allow-Origin', origin)
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE')
   }
   next()
 })
